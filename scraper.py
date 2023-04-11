770024f57b807e54a85628176e0d9258e6160204
MichalFedorek420 <michal.fedorek2@gmail.com>
2023-04-11 15:47:11 +0200

    wiadomosc

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ def get_cos(ancestor, selector = None, attribute = None, return_list = False):
     except AttributeError:
         return None
 
-# product_code = input("Podaj kod produktu: ")
+# product_cod = input("Podaj kod produktu: ")
 product_code = "96685108"
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response = requests.get(url)
